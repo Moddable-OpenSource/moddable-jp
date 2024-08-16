90993cc3f0a62bf5858d28202f5c8c4fda5a1dda
Peter Hoddie <peter@moddable.tech>
2024-08-16 14:40:10 -0700

    log binary data over debug connection

--- tools/xsbug-log/xsbug-logmachine.js
+++ tools/xsbug-log/xsbug-logmachine.js
@@ -1,8 +1,32 @@
-let Machine = require('./xsbug-machine.js').Machine;
+/*
+ * Copyright (c) 2022-2024  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Tools.
+ * 
+ *   The Moddable SDK Tools is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Tools is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU General Public License
+ *   along with the Moddable SDK Tools.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+const { openSync, writeSync } = require('node:fs');
+const { Buffer } = require('node:buffer');
+
+const { Machine } = require('./xsbug-machine.js');
 
 class LogMachine extends Machine {
 	view = {};
 	log = "";
+	binaryOutput = null;
 
 	onTitleChanged(title, tag) {
 		super.onTitleChanged(title, tag);
@@ -15,7 +39,13 @@ class LogMachine extends Machine {
 	onLogged(path, line, data) {
 		this.log += data;
 		if (this.log.endsWith("\n")) {
-			console.log(this.log.slice(0, this.log.length - 1));
+			if (this.log.startsWith("|:") && this.log.endsWith(":|\n")) {
+				if (!this.binaryOutput)
+					this.binaryOutput = openSync(process.env.MODDABLE + "/build/tmp/log.bin", "w+");
+				writeSync(this.binaryOutput, Buffer.from(this.log.slice(2, this.log.length - 4), "base64"), 0);
+			}
+			else
+				console.log(this.log.slice(0, this.log.length - 1));
 			this.log = "";
 		}
 	}
