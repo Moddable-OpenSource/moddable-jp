00e79a0a00ef1e204bda9eb6e260a638101525dd
Peter Hoddie <peter@moddable.com>
2024-09-25 15:05:58 -0700

    remove prehistoric commen

--- build/devices/esp32/xsProj-esp32/main/main.c
+++ build/devices/esp32/xsProj-esp32/main/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2023  Moddable Tech, Inc.
+ * Copyright (c) 2016-2024  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK Runtime.
  * 
@@ -240,7 +240,7 @@ void app_main() {
 #ifdef mxDebug
 	uartConfig.baud_rate = DEBUGGER_SPEED;
 #else
-	uartConfig.baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE;		//@@ different from ESP8266
+	uartConfig.baud_rate = CONFIG_ESP_CONSOLE_UART_BAUDRATE;
 #endif
 	uartConfig.data_bits = UART_DATA_8_BITS;
 	uartConfig.parity = UART_PARITY_DISABLE;
